c0815bc9a72b894e6483ead52590490107405042
sathyakps <sathyakpsandroid@gmail.com>
2017-11-14 00:07:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.get('/', function(req,res) {
 
 
 
-app.listen(process.env.PORT || 5000),function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log('Server Started. App Listening at port 3000')
 })
